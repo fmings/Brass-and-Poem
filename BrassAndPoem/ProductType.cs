862793f87386029062c41b27654e4149cc0df67d
fmings <mingsfelicia@gmail.com>
2024-07-15 18:40:35 -0500

    set up product list

--- BrassAndPoem/ProductType.cs
+++ BrassAndPoem/ProductType.cs
@@ -1,1 +1,9 @@
 //create your ProductType class here
+
+public class ProductType
+{
+    public string Title { get; set; }
+
+    public int Id { get; set; }
+
+}
--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,7 +1,55 @@
 ﻿
 //create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+List<Product> products = new List<Product>()
+{
+    new Product()
+    {
+        Name = "Trumpet",
+        Price = 299.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Trombone",
+        Price = 599.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Tuba",
+        Price = 799.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "The Waste Land",
+        Price = 19.99M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "Milk and Honey",
+        Price = 9.99M,
+        ProductTypeId = 2
+    },
+};
 
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
+
+List<ProductType> productTypes = new List<ProductType>()
+{
+    new ProductType()
+    {
+        Title = "Brass",
+        Id = 1
+    },
+
+    new ProductType()
+    {
+        Title = "Poem",
+        Id = 2
+    },
+};
 
 //put your greeting here
 
